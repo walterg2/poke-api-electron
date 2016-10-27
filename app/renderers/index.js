7b5b4bfad46124cc23092eecf885d8bc68c249c4
George Walters II <george.walters@teamnorthwoods.com>
2016-10-27 12:43:06 -0400

    Gathering Pokemon details

--- app/renderers/index.js
+++ app/renderers/index.js
@@ -1,4 +1,11 @@
 const pokemonList = document.querySelector('.list');
+const details = document.querySelector('.details');
+const pokemon = document.querySelector('.pokemon');
+const form = document.querySelector('.form');
+const height = document.querySelector('.height');
+const weight = document.querySelector('.weight');
+const experience = document.querySelector('.experience');
+const abilities = document.querySelector('.abilities');
 
 let getResponse = (response) => {
   if (response.ok) {
@@ -44,6 +51,28 @@ let processJson = (results) => {
   });
 };
 
+let processAbilities = (items) => {
+  items.forEach((item) => {
+    let listItem = document.createElement('li');
+
+    listItem.innerHTML = item['ability']['name'];
+
+    abilities.appendChild(listItem);
+  });
+};
+
+let processDetails = (jsonObject) => {
+  pokemon.innerHTML = jsonObject['name'];
+  form.src = jsonObject['sprites']['front_default'];
+  height.innerHTML = jsonObject['height'];
+  weight.innerHTML = jsonObject['weight'];
+  experience.innerHTML = jsonObject['base_experience'];
+  
+  processAbilities(jsonObject['abilities']);
+
+  details.style.display = 'block';
+};
+
 let handleError = (error) => {
   alert(`Something went wrong.\n${error.message}`);
 };
@@ -54,3 +83,13 @@ fetch('http://pokeapi.co/api/v2/pokemon/?limit=1000')
   .then(processJson)
   .catch(handleError);
 
+pokemonList.addEventListener('click', (event) => {
+  if (event.target.href) {
+    event.preventDefault();
+
+    fetch(event.target.href)
+      .then(getResponse)
+      .then(processDetails)
+      .catch(handleError);
+  }
+});
